0d0fc8442d3892ded097bd9ff286e11cfabee11f
Fangfei Chen <fangfei1star@gmail.com>
2019-10-20 20:11:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,7 +22,9 @@ def shift_with_args (array) #removes the first 2 items of the array
   array .shift(2)
 end
 
-def using_concat (arr1, arr2)
+def using_concat (arr1, arr2) #.concat = add the contents of the second array to the first,
+                              # increases the lenght of the first array
   arr1 .concat (arr2)
-
 end
+
+
